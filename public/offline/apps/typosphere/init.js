3228dceef3d99593a8dafdc5be070a9ca89af823
Tibor Pino <tibor.pino@gmail.com>
2010-01-07 17:40:50 +0100

    Support for offline browsing. All view files are cached during first request.

--- public/offline/apps/typosphere/init.js
+++ public/offline/apps/typosphere/init.js
@@ -1,10 +1,12 @@
-include.engines();
 include.plugins('model','view','controller','dom/fixtures','dom/form_params','model/validation','view/helpers');
 
 include.resources('sqlite_driver','notification');
 
 include(function(){ //runs after prior includes are loaded
-  include.models('article','category','tag','sidebar','comment','blog');
-  include.controllers('main','article','category','tag','sidebar','link');
-  include.views('article','category','tag');
+    include.models('article','category','tag','sidebar','comment','blog');
+    include.controllers('main','article','category','tag','sidebar');
+    include.views('views/article/show','views/article/archive','views/article/init','views/article/init_archive','views/article/comment','views/article/comment_form','views/article/list',
+                    'views/category/show','views/category/init',
+                    'views/sidebar/archive','views/sidebar/category','views/sidebar/page','views/sidebar/tag',
+                    'views/tag/init','views/tag/show');
 });
