eda70680aa7f7cebbb031a284f84e170adf5130e
Tibor Pino <tibor.pino@gmail.com>
2010-01-12 15:59:57 +0100

    Displays only certain sidebars, because of rails type db column. CategorySidebar, TagSidebar, ArchivesSidebar and PageSidebar are supported.

--- public/offline/controllers/sidebar_controller.js
+++ public/offline/controllers/sidebar_controller.js
@@ -8,44 +8,42 @@ jQuery.Controller.extend('SidebarController',
 },
 /* @Prototype */
 {
-    load: function() {
-        Sidebar.find_all([],this.callback('load_sidebars'))
-    },
-    load_sidebars: function(sidebars) {
-        for(var i=0;i<sidebars.length;i++) {
-            $('#boxes').append($(document.createElement('section')).attr('id',sidebars[i].type))
-        }
+    load: function(sidebars) {
         Category.sidebar([],this.callback('sidebar_category'))
         Tag.sidebar([],this.callback('sidebar_tag'),this.callback(db_con.error))
         Article.sidebar([],this.callback('sidebar_archive'),this.callback(db_con.error))
         Article.sidebar_page([],this.callback('sidebar_page'),this.callback(db_con.error))
     },
     sidebar_category: function(categories) {
-        if($('#CategorySidebar').length) {
-            $("#CategorySidebar").html(this.view('sidebar/category',{
-                categories:categories
-            }))
+        if(!$('#CategorySidebar').length) {
+            $('#boxes').append($(document.createElement('section')).attr('id','CategorySidebar'))
         }
+        $("#CategorySidebar").html(this.view('sidebar/category',{
+            categories:categories
+        }))
     },
     sidebar_tag: function(tags) {
-        if ($('#TagSidebar').length) {
-            $("#TagSidebar").html(this.view('sidebar/tag',{
-                tags:tags
-            }))
+        if (!$('#TagSidebar').length) {
+            $('#boxes').append($(document.createElement('section')).attr('id','TagSidebar'))
         }
+        $("#TagSidebar").html(this.view('sidebar/tag',{
+            tags:tags
+        }))
     },
     sidebar_archive: function(articles) {
-        if ($('#ArchivesSidebar').length) {
-            $("#ArchivesSidebar").html(this.view('sidebar/archive',{
-                articles:articles
-            }))
+        if (!$('#ArchivesSidebar').length) {
+            $('#boxes').append($(document.createElement('section')).attr('id','ArchivesSidebar'))
         }
+        $("#ArchivesSidebar").html(this.view('sidebar/archive',{
+            articles:articles
+        }))
     },
     sidebar_page: function(pages) {
-        if ($('#PageSidebar').length) {
-            $("#PageSidebar").html(this.view('sidebar/page',{
-                pages:pages
-            }))
+        if (!$('#PageSidebar').length) {
+            $('#boxes').append($(document.createElement('section')).attr('id','PageSidebar'))
         }
+        $("#PageSidebar").html(this.view('sidebar/page',{
+            pages:pages
+        }))
     }
 });
