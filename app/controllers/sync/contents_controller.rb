f66a32fee2fa112997b412a663194dea3da23ea7
Tibor Pino <tibor.pino@gmail.com>
2010-04-06 11:24:41 +0200

    Fixes sync error.

--- app/controllers/sync/contents_controller.rb
+++ app/controllers/sync/contents_controller.rb
@@ -1,7 +1,7 @@
 class Sync::ContentsController < Sync::BaseController
 
   def index
-    @articles = Content.find(:all, :limit => 20)
+    @articles = Content.find(:all, :order => 'published_at desc', :limit => 20)
     @articles.each { |article| article.body = article.html(:body)  }
     respond_to do |format|
       format.json { render :json => @articles.to_json(:only => [:id,:type,:title,:author,:body,:extended,:excerpt,:keywords,:created_at,:updated_at,:user_id,:permalink,:guid,:text_filter_id,:whiteboard,:name,:published,:allow_pings,:allow_comments,:published_at]) }
